e72e5492ae0eeccfe0de4f93c6da60bbf9b156ce
TAC <tac@tac42.net>
2015-02-12 14:39:37 +0000

    Fix app

--- app.rb
+++ app.rb
@@ -5,33 +5,30 @@
 
 require 'pp'
 require 'time'
+require 'mechanize'
 
 require_relative 'lib/swim/swim'
 
-@agent = Mechanize.new
+agent = Mechanize.new
 
-# c = Swim::Cond::and( Cond::Match(/\d/), Cond::NewerThen() )
-c = Swim::Cond::Modify("2月11日 19時04分")
-t = Swim::Trigger.new("http://www3.nhk.or.jp/news/", "//*[@id='topttl']/a/span", c)
+c = Swim::Cond::and( Swim::Cond::Match(/\d/), Swim::Cond::NewerThen() )
+t = Swim::Trigger.new("http://www.tac42.net/", '//*[@id="contents"]/dl/dt/span', c)
 
-begin
-  puts Swim.trigger(t, @agent)
-rescue => e
-  puts e.message
-  pp e
-end
+if Swim.trigger(t, agent)
+  root = Swim::LinksNode.new('//*[@id="menu"]/ul/li/a', "root", [
+           Swim::ContentNode.new('//*[@id="contents"]/h2', "title"),
+           Swim::ContentNode.new('//*[@id="contents"]/p[1]', "content"),
+         ]
+  )
 
+  root_page = agent.get("http://www.tac42.net/")
 
-root = Swim::LinksNode.new('//*[@id="menu"]/ul/li/a', "root", [
-         Swim::ContentNode.new('//*[@id="contents"]/h2', "title"),
-         Swim::ContentNode.new('//*[@id="contents"]/p[1]', "content"),
-       ]
-)
-
-root_page = @agent.get("http://www.tac42.net/")
-result = root.inject(@agent, root_page)
-result.each.with_index do |l, idx|
-  puts l["title"]
-  puts l["content"]
-  puts "--" * 10
+  result = root.inject(agent, root_page)
+  result.each do |sub_content|
+    puts sub_content["title"]
+    puts sub_content["content"]
+    puts "--" * 10
+  end
+else
+  puts "page is not modify."
 end
