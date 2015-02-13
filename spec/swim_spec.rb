abca9869789b74ca12155a5b76a163254f538fd2
TAC <tac@tac42.net>
2015-02-13 16:27:58 +0000

    Add LinksNode specs

--- spec/swim_spec.rb
+++ spec/swim_spec.rb
@@ -1,4 +1,3 @@
-#!/usr/bin/env ruby
 # -*- coding: utf-8 -*-
 
 # Author::    TAC (tac@tac42.net)
@@ -21,6 +20,24 @@ describe 'ContentNode' do
       page = @agent.get(@uri)
       actual = @node.inject(@agent, page)
       expect(actual).to eq "Hello,Swim"
+    end
+  end
+
+  describe '::LinksNode' do
+    before { @root_page = @agent.get(@uri) }
+
+    it 'scrape links' do
+      root_node = Swim::LinksNode.new('/html/body/a', "root", [
+        Swim::ContentNode.new('/html/body/p', "content"),
+      ])
+
+      actual = root_node.inject(@agent, @root_page)
+      expected = [
+        {"content" => "Child 01 page."},
+        {"content" => "Child 02 page."},
+        {"content" => "Child 03 page."},
+      ]
+      expect(actual).to match expected
     end
   end
 end
