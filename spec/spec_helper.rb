835fda356dfadd99d319f168e31e78cac8610917
TAC <tac@tac42.net>
2015-02-13 16:00:05 +0000

    Ignore webserver coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,21 +3,6 @@
 
 # Author::    TAC (tac@tac42.net)
 
-ENV['CODECLIMATE_REPO_TOKEN'] = "0dc78d33107a7f11f257c0218ac1a37e0073005bb9734f2fd61d0f7e803fc151"
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
-
-# require 'simplecov'
-# require 'coveralls'
-# Coveralls.wear!
-
-# SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
-#   SimpleCov::Formatter::HTMLFormatter,
-#   Coveralls::SimpleCov::Formatter
-# ]
-# SimpleCov.start
-
-
 require 'glint'
 Dir[File.expand_path("../servers/*.rb", __FILE__)].each {|f| require f}
 
@@ -28,3 +13,18 @@ shared_context 'httpserver' do
     "http://#{Glint::Server.info[:httpserver][:host]}:#{Glint::Server.info[:httpserver][:port]}"
   }
 end
+
+
+# ENV['CODECLIMATE_REPO_TOKEN'] = "0dc78d33107a7f11f257c0218ac1a37e0073005bb9734f2fd61d0f7e803fc151"
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
+
+require 'simplecov'
+require 'coveralls'
+Coveralls.wear!
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+SimpleCov.start
