aa5418a6bb0dbddeddc0dd6b8b1bd4b697d9e054
TAC <tac@tac42.net>
2015-02-17 20:49:06 +0900

    Update example

--- app.rb
+++ app.rb
@@ -11,24 +11,43 @@ require_relative 'lib/swim/swim'
 
 agent = Mechanize.new
 
-c = Swim::Cond::and( Swim::Cond::Match(/\d/), Swim::Cond::NewerThen() )
-t = Swim::Trigger.new("http://www.tac42.net/", '//*[@id="contents"]/dl/dt/span', c)
+uri = "http://www.asahi.com/"
 
-if Swim.trigger(t, agent)
-  root = Swim::LinksNode.new('//*[@id="menu"]/ul/li/a', "root", [
-           Swim::ContentNode.new('//*[@id="contents"]/h2', "title"),
-           Swim::ContentNode.new('//*[@id="contents"]/p[1]', "content"),
-         ]
-  )
-
-  root_page = agent.get("http://www.tac42.net/")
-
-  result = root.inject(agent, root_page)
-  result.each do |sub_content|
-    puts sub_content["title"]
-    puts sub_content["content"]
-    puts "--" * 10
-  end
-else
-  puts "page is not modify."
+root = links_top '//*[@id="MainInner"]/div[1]/ul/li/a' do
+  text_title   '//*[@id="MainInner"]/div[1]/div/h1'
+  text_article '//*[@id="MainInner"]/div/div[@class="ArticleText"]'
 end
+
+page = agent.get(uri)
+contents = root.inject(agent, page)
+
+contents.each do |h|
+  t = h['title']
+  a = h['article']
+
+  puts t
+  puts a
+  puts "=" * 100
+end
+
+# c = Swim::Cond::and( Swim::Cond::Match(/\d/), Swim::Cond::NewerThen() )
+# t = Swim::Trigger.new("http://www.tac42.net/", '//*[@id="contents"]/dl/dt/span', c)
+
+# if Swim.trigger(t, agent)
+#   root = Swim::LinksNode.new('//*[@id="menu"]/ul/li/a', "root", [
+#            Swim::ContentNode.new('//*[@id="contents"]/h2', "title"),
+#            Swim::ContentNode.new('//*[@id="contents"]/p[1]', "content"),
+#          ]
+#   )
+
+#   root_page = agent.get("http://www.tac42.net/")
+
+#   result = root.inject(agent, root_page)
+#   result.each do |sub_content|
+#     puts sub_content["title"]
+#     puts sub_content["content"]
+#     puts "--" * 10
+#   end
+# else
+#   puts "page is not modify."
+# end
