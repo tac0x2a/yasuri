34825fed2e1b3a54c5e0a01b73d6b9a1a46c682c
TAC <tac@tac42.net>
2015-02-18 23:52:19 +0900

    Update examples

--- app.rb
+++ app.rb
@@ -13,11 +13,32 @@ agent = Mechanize.new
 
 uri = "http://www.asahi.com/"
 
+# Node tree constructing by DSL
 root = links_top '//*[@id="MainInner"]/div[1]/ul/li/a' do
   text_title   '//*[@id="MainInner"]/div[1]/div/h1'
   text_article '//*[@id="MainInner"]/div/div[@class="ArticleText"]'
 end
 
+# Node tree constructing by JSON
+src = <<-EOJSON
+   { "node"     : "links",
+     "name"     : "root",
+     "path"     : "//*[@id='MainInner']/div[1]/ul/li/a",
+     "children" : [
+                    { "node" : "text",
+                      "name" : "title",
+                      "path" : "//*[@id='MainInner']/div[1]/div/h1"
+                    },
+                    { "node" : "text",
+                      "name" : "article",
+                      "path" : "//*[@id='MainInner']/div/div[@class='ArticleText']"
+                    }
+                  ]
+   }
+EOJSON
+root = Swim.json2tree(src)
+
+# Access to parsed resources
 page = agent.get(uri)
 contents = root.inject(agent, page)
 
