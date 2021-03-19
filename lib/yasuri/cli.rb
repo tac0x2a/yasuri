586af9afb4871346c5ef049ac9ee37db2146310e
TAC <tac@tac42.net>
2021-03-19 23:11:35 +0900

    WIP sample implements scrape command

--- lib/yasuri/cli.rb
+++ lib/yasuri/cli.rb
@@ -1,10 +1,21 @@
-require "thor"
+require 'thor'
+require 'json'
+require 'yasuri'
+require 'mechanize'
 
 module YasuriUtils
   class CLI < Thor
-    desc "sample", "sample script"
-    def sample(str)
-      puts "Hello, Yasuri. Hello, #{str}"
+    desc "scrape <URI> <JSON>", "Getting from <URI> and scrape with <JSON>. <JSON> is Yasuri's format json string."
+    def scrape(uri, json)
+      src = json
+
+      root = Yasuri.json2tree(src)
+      agent = Mechanize.new
+      root_page = agent.get(uri)
+      result = root.inject(agent, root_page)
+
+      STDOUT.puts result
+      return 0
     end
   end
 end
