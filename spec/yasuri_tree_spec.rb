3c5416bb5eff55cba61cf50936be482d4ab6216d
TAC <tac@tac42.net>
2021-03-20 12:15:22 +0900

    Add test case: links node inside of tree

--- spec/yasuri_tree_spec.rb
+++ spec/yasuri_tree_spec.rb
@@ -44,5 +44,33 @@ describe 'Yasuri' do
       }
       expect(actual).to include expected
     end
+
+    it "scrape with links node" do
+      tree = Yasuri.tree_sample do
+        tree_group1 do
+          links_a '/html/body/a' do
+            text_content '/html/body/p'
+          end
+          text_child01  '/html/body/a[1]'
+        end
+        tree_group2 do
+          text_child03 '/html/body/a[3]'
+        end
+      end
+      actual = tree.inject(@agent, @index_page)
+
+      expected = {
+        "group1" => {
+          "a" => [
+            {"content" => "Child 01 page."},
+            {"content" => "Child 02 page."},
+            {"content" => "Child 03 page."},
+          ],
+          "child01" => "child01"
+        },
+        "group2" => { "child03" => "child03" }
+      }
+      expect(actual).to include expected
+    end
   end
 end
