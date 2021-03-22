f07ebfead9271c757193427d49c678b9b7efbd48
TAC <tac@tac42.net>
2021-03-22 19:53:38 +0900

    Improve rename variable more concrete

--- lib/yasuri/yasuri_map_node.rb
+++ lib/yasuri/yasuri_map_node.rb
@@ -21,32 +21,30 @@ module Yasuri
     end
 
     def to_h
-      h = {}
-      h["node"] = "map"
-      h["name"] = self.name
-      h["children"] = self.children.map{|c| c.to_h} if not children.empty?
+      node_hash = {}
+      node_hash["node"] = "map".freeze
+      node_hash["name"] = self.name
+      node_hash["children"] = self.children.map{|c| c.to_h} if not children.empty?
 
       self.opts.each do |key,value|
-        h[key] = value if not value.nil?
+        node_hash[key] = value if not value.nil?
       end
 
-      h
+      node_hash
     end
 
-    def self.hash2node(node_h)
-      reserved_keys = %i|node name children|
+    def self.hash2node(node_hash)
+      reserved_keys = %i|node name children|.freeze
 
-      node, name, children = reserved_keys.map do |key|
-        node_h[key]
-      end
+      node, name, children = reserved_keys.map{|key| node_hash[key]}
 
       fail "Not found 'name' value in map" if name.nil?
       fail "Not found 'children' value in map" if children.nil?
       children ||= []
 
       childnodes = children.map{|c| Yasuri.hash2node(c) }
-      reserved_keys.each{|key| node_h.delete(key)}
-      opt = node_h
+      reserved_keys.each{|key| node_hash.delete(key)}
+      opt = node_hash
 
       self.new(name, childnodes, **opt)
     end
