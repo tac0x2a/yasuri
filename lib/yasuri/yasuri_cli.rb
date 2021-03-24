1c2fbcae89575ca2054a56b029d91931bf386f1a
TAC <tac@tac42.net>
2021-03-25 01:30:32 +0900

    Implement cli yasuri

--- lib/yasuri/yasuri_cli.rb
+++ lib/yasuri/yasuri_cli.rb
@@ -5,14 +5,49 @@ require 'mechanize'
 
 module Yasuri
   class CLI < Thor
-    desc "scrape <URI> <JSON>", "Getting from <URI> and scrape with <JSON>. <JSON> is Yasuri's format json string."
-    def scrape(uri, json)
-      src = json
+    desc "scrape <URI> [[--file <TREE_FILE>] or [--json <JSON>]]", "Getting from <URI> and scrape it. with <JSON> or json/yml from <TREE_FILE>. They should be Yasuri's format json or yaml string."
+    option :file
+    option :json
+    def scrape(uri)
+      # argument validations
+      if [options[:file], options[:json]].compact.count != 1
+        $stderr.puts "ERROR: Only one of `--file` or `--json` option should be specified."
+        return -1
+      end
+      if options[:file]&.empty? or options[:file] == "file" or options[:json]&.empty?
+        $stderr.puts "ERROR: --file option require not empty argument."
+        return -1
+      end
+      if options[:json]&.empty? or options[:json] == "json"
+        $stderr.puts "ERROR: --json option require not empty argument."
+        return -1
+      end
 
-      root = Yasuri.json2tree(src)
+      tree = if options[:file]
+              src = File.read(options[:file])
+
+              begin
+                Yasuri.json2tree(src)
+              rescue
+                begin
+                  Yasuri.yaml2tree(src)
+                rescue => e
+                  $stderr.puts "ERROR: Failed to convert to yasuri tree `#{options[:file]}`. #{e.message}"
+                  return -1
+                end
+              end
+            else
+              begin
+                Yasuri.json2tree(options[:json])
+              rescue => e
+                $stderr.puts "ERROR: Failed to convert json to yasuri tree. #{e.message}"
+                return -1
+              end
+            end
+
       agent = Mechanize.new
       root_page = agent.get(uri)
-      result = root.inject(agent, root_page)
+      result = tree.inject(agent, root_page)
 
       if result.instance_of?(String)
         puts result
--- spec/yasuri_cli_spec.rb
+++ spec/yasuri_cli_spec.rb
@@ -6,19 +6,78 @@ describe 'Yasuri' do
   before do
     @agent = Mechanize.new
     @index_page = @agent.get(uri)
+
+    @res_dir = File.expand_path('../cli_resources', __FILE__)
   end
 
   describe 'cli scrape' do
+    it "require --file or --json option" do
+      expect {
+        Yasuri::CLI.new.invoke(:scrape, [uri], {})
+      }.to output("ERROR: Only one of `--file` or `--json` option should be specified.\n").to_stderr
+    end
+
+    it "only one of --file or --json option" do
+      expect {
+        Yasuri::CLI.new.invoke(:scrape, [uri], {file: "path.json", json: '{"text_title": "/html/head/title"}'})
+      }.to output("ERROR: Only one of `--file` or `--json` option should be specified.\n").to_stderr
+    end
+
+    it "require --file option is not empty string" do
+      expect {
+        Yasuri::CLI.new.invoke(:scrape, [uri], {file: "file"})
+      }.to output("ERROR: --file option require not empty argument.\n").to_stderr
+    end
+
+    it "require --json option is not empty string" do
+      expect {
+        Yasuri::CLI.new.invoke(:scrape, [uri], {json: "json"})
+      }.to output("ERROR: --json option require not empty argument.\n").to_stderr
+    end
+
+
     it "display text node as simple string" do
       expect {
-        Yasuri::CLI.new.invoke(:scrape, [uri, '{"text_title": "/html/head/title"}'])
+        Yasuri::CLI.new.invoke(:scrape, [uri], {json: '{"text_title": "/html/head/title"}'})
       }.to output("Yasuri Test\n").to_stdout
     end
 
     it "display texts in single json" do
       expect {
-        Yasuri::CLI.new.invoke(:scrape, [uri, '{"text_c1":"/html/body/p[1]", "text_c2":"/html/body/p[2]"}'])
+        Yasuri::CLI.new.invoke(:scrape, [uri], {json: '{"text_c1":"/html/body/p[1]", "text_c2":"/html/body/p[2]"}'})
       }.to output('{"c1":"Hello,Yasuri","c2":"Last Modify - 2015/02/14"}'+"\n").to_stdout
+    end
+
+
+    it "display text node as simple string via json file" do
+      expect {
+        Yasuri::CLI.new.invoke(:scrape, [uri], {file: "#{@res_dir}/tree.json"})
+      }.to output('[{"content":"Hello,YasuriLast Modify - 2015/02/14"}]' + "\n").to_stdout
+    end
+    it "display text node as simple string via yaml file" do
+      expect {
+        Yasuri::CLI.new.invoke(:scrape, [uri], {file: "#{@res_dir}/tree.yml"})
+      }.to output('[{"content":"Hello,YasuriLast Modify - 2015/02/14"}]' + "\n").to_stdout
+    end
+
+
+    it "display ERROR when json string is wrong" do
+      wrong_json = '{,,}'
+      expect {
+        Yasuri::CLI.new.invoke(:scrape, [uri], {json: wrong_json})
+      }.to output("ERROR: Failed to convert json to yasuri tree. 809: unexpected token at '#{wrong_json}'\n").to_stderr
+    end
+    it "display ERROR when json file contains is wrong" do
+      file_path = "#{@res_dir}/tree_wrong.json"
+      expect {
+        Yasuri::CLI.new.invoke(:scrape, [uri], {file: file_path})
+      }.to output("ERROR: Failed to convert to yasuri tree `#{file_path}`. (<unknown>): did not find expected node content while parsing a flow node at line 2 column 3\n").to_stderr
+    end
+    it "display ERROR when yaml file contains is wrong" do
+      file_path = "#{@res_dir}/tree_wrong.yml"
+      expect {
+        Yasuri::CLI.new.invoke(:scrape, [uri], {file: file_path})
+      }.to output("ERROR: Failed to convert to yasuri tree `#{file_path}`. (<unknown>): did not find expected node content while parsing a block node at line 1 column 1\n").to_stderr
     end
   end
 end
